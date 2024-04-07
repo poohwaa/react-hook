1c07767c8f9e3399ada7dd69040ad7f146e23818
poohwaa <poohwaa@gmail.com>
2024-04-07 13:50:39 +0900

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const useInput = (initVal, validator) => {
+    const [value, setValue] = useState(initVal);
+    const onChange = (event) => {
+        // console.log(event.target);
+        const {
+            target: { value },
+        } = event;
+
+        let willUpdate = true;
+        if (typeof validator === "function") {
+            willUpdate = validator(value);
+        }
+        if (willUpdate) {
+            setValue(value);
+        }
+    };
+
+    return { value, onChange };
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const maxLen = (value) => value.length <= 10;
+    const name = useInput("Mr.", maxLen);
+    return (
+        <div className="App">
+            <h1>React Hook</h1>
+            <input placeholder="Name" {...name} />
+        </div>
+    );
 }
 
 export default App;
